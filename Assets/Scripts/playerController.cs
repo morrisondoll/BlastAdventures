7d62cbdd09ab4e545af38d69f7ac022ade2e0a81
Jesús Israel Trejo Jiménez <jesustrejo@kybel.mx>
2018-09-25 09:54:47 -0500

    Archivos Base
    Contiene unos prefabs base, player, camera y un escenario, ya viene incorporado Tiled2Unity

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	//Atributos de la clase
+	public float maxSpeed = 10f;
+	public float speed = 20f;
+	private Rigidbody2D rbPlayer;
+	private Animator animPlayer;
+
+	// Use this for initialization
+	void Start () {
+		rbPlayer = GetComponent<Rigidbody2D>();
+		animPlayer = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis("Horizontal");
+
+		rbPlayer.AddForce(Vector2.right*speed*h);
+
+	}
+}
---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	//Atributos de la clase
+	public GameObject follow;
+	public Vector2 minCamPosition, maxCamPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float posX = follow.transform.position.x;
+		float posY = follow.transform.position.y;
+
+    Debug.Log("x: "+posX);
+		Debug.Log("x: "+posY);
+		transform.position = new Vector3(
+			Mathf.Clamp(posX, minCamPosition.x, maxCamPosition.x),
+      Mathf.Clamp(posY, minCamPosition.y, maxCamPosition.y), 
+			transform.position.z
+		);
+	}
+}
